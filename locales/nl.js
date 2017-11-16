ed1d77452360f4c6bebd8bce0c1c8227c094c852
Marten Veldthuis <marten@veldthuis.com>
2017-11-16 12:09:31 +0000

    Add Dutch locale file

---
+++ locales/nl.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  damals: {
+    just_now: 'zojuist',
+
+    less_than_x_seconds_ago: {
+      one:   'minder dan een seconde geleden',
+      other: 'minder dan %(count)s seconden geleden'
+    },
+
+    half_a_minute_ago: 'een halve minuut geleden',
+
+    less_than_x_minutes_ago: {
+      one:   'minder dan een minuut geleden',
+      other: 'minder dan %(count)s minuten geleden'
+    },
+
+    x_minutes_ago: {
+      one:   'een minuut geleden',
+      other: '%(count)s minuten geleden'
+    },
+
+    about_x_hours_ago: {
+      one:   'ongeveer een uur geleden',
+      other: 'ongeveer %(count)s uur geleden'
+    },
+
+    x_days_ago: {
+      one:   'één dag geleden',
+      other: '%(count)s dagen geleden'
+    },
+
+    about_x_months_ago: {
+      one:   'ongeveer een maand geleden',
+      other: 'ongeveer %(count)s maanden geleden'
+    },
+
+    x_months_ago: {
+      one:   'één maand geleden',
+      other: '%(count)s maanden geleden'
+    },
+
+    about_x_years_ago: {
+      one:   'ongeveer een jaar geleden',
+      other: 'ongeveer %(count)s jaar geleden'
+    },
+
+    over_x_years_ago: {
+      one:   'meer dan een jaar geleden',
+      other: 'meer dan %(count)s jaar geleden'
+    },
+
+    almost_x_years_ago: {
+      one:   'bijna een jaar geleden',
+      other: 'bijna %(count)s jaar geleden'
+    }
+  }
+};
